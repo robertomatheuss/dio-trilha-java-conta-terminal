54229612e3e24b8aa3901c94e0a5bad904d46a5e
Roberto Matheus <robertmatheusa@gmail.com>
2024-04-25 17:23:07 -0300

    Adicionando modulo try-catch

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("Digite o nome: ");
-        String nome = scanner.nextLine();
-        
-        System.out.println("Digite a agencia da conta: ");
-        String agencia = scanner.nextLine();
-        
-        System.out.println("Digite o numero da conta: ");
-        Integer numero = scanner.nextInt();
-
-        System.out.println("Digite o saldo da conta: ");
-        Double saldo = scanner.nextDouble();
-
-        Conta conta = new Conta(numero, agencia, nome, saldo);
-        
+        try(Scanner scanner = new Scanner(System.in).useLocale(Locale.US)){
+            System.out.println("Digite o nome: ");
+            String nome = scanner.nextLine();
+            
+            System.out.println("Digite a agencia da conta: ");
+            String agencia = scanner.nextLine();
+            
+            System.out.println("Digite o numero da conta: ");
+            Integer numero = scanner.nextInt();
+    
+            System.out.println("Digite o saldo da conta: ");
+            Double saldo = scanner.nextDouble();
+    
+            Conta conta = new Conta(numero, agencia, nome, saldo);    
+        }catch(Exception e ){
+            System.out.println(e.getMessage());
+        }
     }
 }
